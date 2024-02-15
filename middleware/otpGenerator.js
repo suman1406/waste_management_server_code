1a812d2495b0bdc2dd93eef11c3dce68cb7f336e
Suman Panigrahi <psuman1406@gmail.com>
2024-02-15 21:15:08 +0530

    Token generation files coded

---
+++ middleware/otpGenerator.js
@@ -0,0 +1,10 @@
+const otpGenerator = require('otp-generator');
+
+const otpGenerator = () => {
+    return otpGenerator.generate(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+        digits: true
+    });
+}
